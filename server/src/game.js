434d1c4362a8339f3d7300aee6a280c13b7c4369
Alex King <alex.king@and.digital>
2020-11-12 00:04:12 +0000

    Add core Red or Black game logic
    Still to do:
     - Testing
     - Player statuses

---
+++ server/src/game.js
@@ -0,0 +1,246 @@
+import { RED_OR_BLACK } from './constants/games';
+import { PLAYER_ACTION_REQUEST } from './constants/messages';
+import {
+  BLACK,
+  CLUBS,
+  DIAMONDS,
+  FUCK_YOU,
+  GIVE_DRINK,
+  HEARTS,
+  INBETWEEN,
+  LOWER,
+  RED,
+  RIDE_THE_BUS,
+  SAME,
+  SPADES,
+  TAKE_DRINK
+} from './constants/statuses';
+
+export const populateDeck = (numberOfDecks) => {
+  const suits = [HEARTS, SPADES, CLUBS, DIAMONDS];
+  const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+
+  const deck = [];
+  for (let i = 0; i < numberOfDecks; i++) {
+    suits.forEach((suit) => {
+      values.forEach((value) => {
+        deck.push({ suit, value });
+      });
+    });
+  }
+
+  const jokers = Array(2 * numberOfDecks).fill({ suit: null, value: 'JOKER' });
+
+  return [...deck, ...jokers];
+};
+
+export const initialisePlayer = (playerInitRequest, gameState, ws) => {
+  ws.name = playerInitRequest.name;
+  const { name, id } = ws;
+  gameState.public.players.push({
+    name,
+    id,
+    status: null
+  });
+  gameState.private.playerCards[id] = [];
+  if (!gameState.public.currentPlayer) gameState.public.currentPlayer = id;
+  const playerInitAck = {
+    type: PLAYER_INIT_ACK,
+    payload: { id, gameState: gameState.public }
+  };
+  ws.send(JSON.stringify(playerInitAck));
+};
+
+export const broadcastGameState = (gameState, clients) => {
+  clients.forEach((client) => {
+    const msgObject = {
+      type: GAME_STATE,
+      payload: {
+        gameState: gameState.public,
+        cards: gameState.private.playerCards[client.id]
+      }
+    };
+    const msgString = JSON.stringify(msgObject);
+    client.send(msgString);
+  });
+  console.debug('game state broadcast to all clients');
+};
+
+export const removePlayer = (gameState, ws, clients) => {
+  // Remove from client list
+  clients.splice(
+    clients.findIndex((client) => client.id === ws.id),
+    1
+  );
+
+  // Return if client was not in player list
+  const isPlayer = gameState.public.players.find((player) => player.id === ws.id);
+  if (!isPlayer) return;
+
+  // If leaving player is next up, move on to player after
+  if (ws.id === gameState.public.currentPlayer) {
+    gameState.public.currentPlayer =
+      gameState.public.players[
+        (gameState.public.players.findIndex((player) => player.id === ws.id) + 1) % gameState.public.players.length
+      ].id;
+  }
+
+  // Remove leaving player from player list
+  gameState.public.players.splice(
+    gameState.public.players.findIndex((player) => player.id === ws.id),
+    1
+  );
+};
+
+export const restartGame = (gameState, numberOfDecks) => {
+  gameState.public = {
+    players: [],
+    game: RED_OR_BLACK,
+    currentPlayer: null
+  };
+  gameState.private = {
+    playerCards: {},
+    deck: populateDeck(numberOfDecks)
+  };
+};
+
+export const updateCurrentGame = (gameState) => {
+  switch (gameState.public.game) {
+    case RED_OR_BLACK:
+      const playerIds = gameState.public.players.map((player) => player.id);
+      const fullHands = playerIds.filter((id) => gameState.private.playerCards[id].length === 5);
+      if (fullHands.length === playerIds.length) {
+        gameState.public.game = FUCK_YOU;
+        gameState.public.currentPlayer = gameState.public.players[0].id;
+        return FUCK_YOU;
+      }
+      break;
+    case FUCK_YOU:
+      const playerIds = gameState.public.players.map((player) => player.id);
+      const nonEmptyHands = playerIds.filter((id) => gameState.private.playerCards[id].length > 0);
+      if (nonEmptyHands.length === 1) {
+        gameState.public.game = RIDE_THE_BUS;
+        gameState.public.currentPlayer = nonEmptyHands[0];
+        gameState.private.deck = populateDeck(1);
+        return RIDE_THE_BUS;
+      }
+      break;
+    default:
+      break;
+  }
+};
+
+export const cardValueToNumericalValue = (cardValue) => {
+  const numericalValues = {
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 7,
+    8: 8,
+    9: 9,
+    10: 10,
+    J: 11,
+    Q: 12,
+    K: 13,
+    A: 14,
+    JOKER: -1
+  };
+  return numericalValues[cardValue];
+};
+
+export const sendPlayerActionRequest = (gameState, action, ws) => {
+  const msgObject = {
+    type: PLAYER_ACTION_REQUEST,
+    payload: {
+      gameState: gameState.public,
+      cards: gameState.private.playerCards[ws.id],
+      action
+    }
+  };
+  const msgString = JSON.stringify(msgObject);
+  client.send(msgString);
+  console.debug(`client ${ws.id}: ${action} request sent`);
+};
+
+export const nextPlayer = (gameState) => {
+  gameState.public.currentPlayer =
+    gameState.public.players[
+      (gameState.players.findIndex((player) => player.id === ws.id) + 1) % gameState.players.length
+    ].id;
+};
+
+export const handleRedOrBlackChoice = (gameState, choice, ws) => {
+  const playerId = ws.id;
+  const drawnCard = gameState.deck.splice(Math.floor(Math.random() * gameState.private.deck.length), 1)[0];
+  const playerCards = gameState.private.playerCards[playerId];
+  const playerHoldsJoker = playerCards.find((card) => card.value === 'JOKER');
+
+  switch (gameState.private.playerCards[playerId].length) {
+    case 0:
+      gameState.private.playerCards[playerId].push(drawnCard);
+      if (
+        (choice === RED && (drawnCard.suit === HEARTS || drawnCard.suit === DIAMONDS)) ||
+        (choice === BLACK && (drawnCard.suit === SPADES || drawnCard.suit === CLUBS))
+      ) {
+        sendPlayerActionRequest(gameState, GIVE_DRINK, ws);
+      } else {
+        sendPlayerActionRequest(gameState, TAKE_DRINK, ws);
+      }
+      break;
+    case 1:
+      const drawnCardValue = cardValueToNumericalValue(drawnCard.value);
+      const playerCardValue = cardValueToNumericalValue(playerCards[0].value);
+      gameState.private.playerCards[playerId].push(drawnCard);
+      if (
+        ((choice === HIGHER && drawnCardValue > playerCardValue) ||
+          (choice === LOWER && drawnCardValue < playerCardValue) ||
+          (choice === SAME && drawnCardValue < playerCardValue)) &&
+        !playerHoldsJoker
+      ) {
+        sendPlayerActionRequest(gameState, GIVE_DRINK, ws);
+      } else {
+        sendPlayerActionRequest(gameState, TAKE_DRINK, ws);
+      }
+      break;
+    case 2:
+      const drawnCardValue = cardValueToNumericalValue(drawnCard.value);
+      const sortedPlayerCardValues = playerCards.map((card) => cardValueToNumericalValue(card.value)).sort();
+      const lowerPlayerCardValue = sortedPlayerCardValues[0];
+      const upperPlayerCardValue = sortedPlayerCardValues[1];
+      gameState.private.playerCards[playerId].push(drawnCard);
+      if (
+        ((choice === OUTSIDE && (drawnCardValue < lowerPlayerCardValue || drawnCardValue > upperPlayerCardValue)) ||
+          (choice === INBETWEEN && drawnCardValue > lowerPlayerCardValue && drawnCardValue < upperPlayerCardValue)) &&
+        !playerHoldsJoker
+      ) {
+        sendPlayerActionRequest(gameState, GIVE_DRINK, ws);
+      } else {
+        sendPlayerActionRequest(gameState, TAKE_DRINK, ws);
+      }
+      break;
+    case 3:
+      gameState.private.playerCards[playerId].push(drawnCard);
+      if (
+        (choice === SPADES && drawnCard.suit === SPADES) ||
+        (choice === CLUBS && drawnCard.suit === CLUBS) ||
+        (choice === DIAMONDS && drawnCard.suit === DIAMONDS) ||
+        (choice === HEARTS && drawnCard.suit === HEARTS)
+      ) {
+        sendPlayerActionRequest(gameState, GIVE_DRINK, ws);
+      } else {
+        sendPlayerActionRequest(gameState, TAKE_DRINK, ws);
+      }
+      break;
+    case 4:
+      if (drawnCard.value === choice.value && drawnCard.suit === choice.suit) {
+        sendPlayerActionRequest(gameState, GIVE_DRINK, ws);
+      } else {
+        sendPlayerActionRequest(gameState, TAKE_DRINK, ws);
+      }
+      break;
+    default:
+      break;
+  }
+};
--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,22 @@
-import { } from 'dotenv/config.js';
+import {} from 'dotenv/config.js';
 import fs from 'fs';
 import https from 'https';
+import readline from 'readline';
 import WebSocket from 'ws';
+import { EXIT, HELP, REMOVE_PLAYER, RESTART, SET_DECKS, SKIP, UPDATE } from './constants/commands.js';
+import { PLAYER_ACTION_RESPONSE, PLAYER_INIT, SERVER_ERROR } from './constants/messages.js';
+import { CHOOSE, FUCK_YOU, RED_OR_BLACK, WAITING_FOR_PLAYER } from './constants/statuses.js';
+import {
+  broadcastGameState,
+  handleRedOrBlackChoice,
+  initialisePlayer,
+  nextPlayer,
+  populateDeck,
+  removePlayer,
+  restartGame,
+  sendPlayerActionRequest,
+  updateCurrentGame
+} from './game.js';
 
 // SERVER
 let PORT = 8080;
@@ -21,9 +36,35 @@ if (process.env.USE_HTTPS_SERVER === 'true') {
 }
 const wss = new WebSocket.Server(wssConfig);
 
+const sendServerError = (errorMessage, clients) => {
+  const msgObject = {
+    type: SERVER_ERROR,
+    payload: {
+      errorMessage
+    }
+  };
+  const msgString = JSON.stringify(msgObject);
+  clients.forEach((client) => client.send(msgString));
+  console.debug(
+    `server error with message ${errorMessage} sent to client(s) ${clients.map((client) => client.id).join()}`
+  );
+};
+
 // GLOBALS
 const clients = [];
+let numberOfDecks = 1;
 let idCounter = 1;
+const gameState = {
+  public: {
+    players: [],
+    game: RED_OR_BLACK,
+    currentPlayer: null
+  },
+  private: {
+    playerCards: {},
+    deck: populateDeck(numberOfDecks)
+  }
+};
 
 // WEBSOCKET LISTENERS
 wss.on('connection', (ws) => {
@@ -35,11 +76,65 @@ wss.on('connection', (ws) => {
   });
 
   ws.on('message', (reqString) => {
-    console.debug(`client ${ws.id}: ${reqString}`);
+    const req = JSON.parse(reqString);
+    console.log(req);
+    switch (req.type) {
+      case PLAYER_INIT:
+        const duplicateName = gameState.public.players.find((player) => player.name === req.payload.name);
+        if (duplicateName) {
+          sendServerError(
+            `Player with name "${req.payload.name}" has already joined - please choose a different name`,
+            [ws]
+          );
+          break;
+        }
+
+        initialisePlayer(req.payload, gameState, ws);
+        console.debug(`client ${ws.id}: player initialised with name ${ws.name}`);
+        broadcastGameState(gameState, clients);
+        break;
+      case PLAYER_ACTION_RESPONSE:
+        if (ws.id !== gameState.public.currentPlayer) break;
+
+        switch (gameState.public.game) {
+          case RED_OR_BLACK:
+            if (req.payload.choice) {
+              handleRedOrBlackChoice(gameState, choice, ws);
+            } else {
+              nextPlayer(gameState);
+              updateCurrentGame(gameState);
+              sendPlayerActionRequest(
+                gameState,
+                CHOOSE,
+                clients.find((client) => client.id === gameState.public.currentPlayer)
+              );
+            }
+            break;
+          case FUCK_YOU:
+            // TODO
+            break;
+          default:
+            break;
+        }
+
+        broadcastGameState(gameState, clients);
+        break;
+      default:
+        break;
+    }
   });
 
   ws.on('close', () => {
-    console.debug(`client ${ws.id}: connection closed`);
+    removePlayer(gameState, ws, clients);
+    console.debug(`client ${ws.id}: connection closed, removed from game`);
+
+    if (!gameState.players.length) {
+      restartGame(gameState, numberOfDecks);
+      idCounter = 1;
+      console.debug(`no players remaining, game state reset`);
+    }
+
+    broadcastGameState(gameState, clients);
   });
 
   ws.on('error', (err) => {
@@ -49,6 +144,61 @@ wss.on('connection', (ws) => {
 
 wss.on('error', (err) => {
   console.debug(`server encountered an error: ${err.name} - ${err.message}`);
+});
+
+// SERVER CLI
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+console.info(`Server running on port ${PORT}`);
+console.info('Type `start` to begin the game');
+console.info('Type `help` to see a list of commands.');
+
+rl.on('line', (input) => {
+  const inputSplit = input.split(' ');
+  const command = inputSplit[0].toLowerCase();
+  const parameter = input.split.length > 1 ? inputSplit[1] : null;
+  switch (command) {
+    case HELP:
+      console.info('Available commands:');
+      console.info("  `skip` - skip the current player's turn.");
+      console.info('  `update` - broadcast the current game state to all players.');
+      console.info('  `restart` - restart the game.');
+      console.info('  `set-decks <number_of_decks>` - update the number of decks in play.');
+      console.info('  `remove-player <player_id>` - remove a player from the game. [TODO - Not Yet Implemented]');
+      console.info('  `exit` - close the server.');
+      break;
+    case SKIP:
+      // TODO
+      break;
+    case EXIT:
+      console.info('Closing server.');
+      wss.close();
+      process.exit(0);
+    case UPDATE:
+      // TODO
+      break;
+    case RESTART:
+      // TODO
+      break;
+    case SET_DECKS:
+      if (!parameter || isNaN(parseInt(parameter))) {
+        console.info('No valid selection given.');
+      } else {
+        numberOfDecks = parseInt(parameter);
+      }
+      break;
+    case REMOVE_PLAYER:
+      // TODO
+      break;
+    case '':
+      break;
+    default:
+      console.info('Command not recognised.');
+      break;
+  }
 });
 
 if (httpsServer) {
